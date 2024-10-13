fc3308d26ce51f1f45f53373566822e4623a3db4
1637497 <91468702+1637497@users.noreply.github.com>
2024-10-13 18:00:02 +0200

    Update and rename crearcsvresultats to crearcsvresultats.py

--- crearcsvresultats.py
+++ crearcsvresultats.py
@@ -1,4 +1,3 @@
-
 import cv2
 import pandas as pd
 import numpy as np
@@ -8,6 +7,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 import os
+import joblib
 
 
 def imatge_binaritzada(imatge):
@@ -18,55 +18,51 @@ def imatge_binaritzada(imatge):
     # plt.show()
     return imatge_otsu
 
+
 def detectar_contorns(imatge_binaria):
     contornos, _ = cv2.findContours(imatge_binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     imatge_contorns = cv2.cvtColor(imatge_binaria, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(imatge_contorns, contornos, -1, (0, 255, 0), 2)
     # plt.imshow(imatge_contorns)
     # plt.title('Tots els contorns detectats')
-    # plt.show()
-    
+    # plt.show() 
     return contornos
+
 
 def retallar_contorn_matricula(imatge, contorns):
     contorn_matricula = None
-    max_area = 0
-    
+    max_area = 0  
     for contorn in contorns:
         x, y, w, h = cv2.boundingRect(contorn)
-        area = w * h
-        
+        area = w * h     
         # Proporció Amplada x Alçada per poder filtrar els contorns que no siguin rectangulars
-        aspect_ratio = float(w) / h
-        
+        aspect_ratio = float(w) / h    
         # Filtrar contorns que tinguin entre 2 i 6 de aspect ratio ( mides arbitraries ) i que tinguin l'area més gran	
         if 2 < aspect_ratio < 6 and area > max_area:
             contorn_matricula = contorn
-            max_area = area
-    
+            max_area = area   
     imatge_resultat = imatge.copy()
-    cv2.drawContours(imatge_resultat, [contorn_matricula], -1, (0, 255, 0), 2)
-    
+    cv2.drawContours(imatge_resultat, [contorn_matricula], -1, (0, 255, 0), 2)    
     plt.imshow(cv2.cvtColor(imatge_resultat, cv2.COLOR_BGR2RGB))
     plt.title('Contorn de la matrícula')
     plt.show()
-
     return contorn_matricula
+
 
 def retallar_matricula(imatge, contorn_matricula):
     x, y, w, h = cv2.boundingRect(contorn_matricula)
-    matricula = imatge[y:y+h, x:x+w]
-    
+    matricula = imatge[y:y+h, x:x+w]    
     # plt.imshow(cv2.cvtColor(matricula, cv2.COLOR_BGR2RGB))
     # plt.title('Matrícula')
     # plt.show()
-    
     return matricula
+
 
 def calcular_lluminositat(img):
     hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     lluminositat = np.mean(hls_img[:, :, 1])  # Canal L
     return lluminositat
+
 
 def eliminar_soroll(imatge,contorns):
     ll=[]
@@ -79,15 +75,11 @@ def eliminar_soroll(imatge,contorns):
         x, y, w, h = cv2.boundingRect(contorn)
         lletra = matricula[y:y+h, x:x+w]
         area = cv2.contourArea(contorn)
-        l=calcular_lluminositat(lletra)
-        
-        
+        l=calcular_lluminositat(lletra)    
         # if min_area < area < max_area and x > 20 and x + w < base_imatge - 20 and y > 20 and y + h < altura_imatge - 20 and h>w:
         if min_area < area < max_area and h>w and x + w < base_imatge - 10:
             contorn_filtrat.append(contorn)
-            ll.append(l)
-           
-    
+            ll.append(l) 
     if len(contorn_filtrat)>7:
         min1, min2 = sorted(ll)[:2]
         diferencia = abs(min1 - min2)
@@ -96,27 +88,22 @@ def eliminar_soroll(imatge,contorns):
             del contorn_filtrat[pos]
         else:
     # if len(contorn_filtrat)>7:
-            contorn_filtrat=contorn_filtrat[1:]
-    
+            contorn_filtrat=contorn_filtrat[1:]  
     return contorn_filtrat
 
-def segmentar_matricula(matricula):
-    
+
+def segmentar_matricula(matricula):    
     matricula_gris = cv2.cvtColor(matricula, cv2.COLOR_BGR2GRAY)
     _, matricula_binaria = cv2.threshold(matricula_gris, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     matricula_binaria = cv2.bitwise_not(matricula_binaria)
-    contorns, _ = cv2.findContours(matricula_binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+    contorns, _ = cv2.findContours(matricula_binaria, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    
     # Descarta els contorns menors de 400 píxels
     # contorns = [contorn for contorn in contorns if cv2.contourArea(contorn) > 400]
-    contorns = [contorn for contorn in contorns if cv2.contourArea(contorn) > 100]
-    
+    contorns = [contorn for contorn in contorns if cv2.contourArea(contorn) > 100]    
     # Ordenar els contorns de esquerra a dreta
     contorns = sorted(contorns, key=lambda contorn: cv2.boundingRect(contorn)[0])
     contorns= eliminar_soroll(matricula, contorns)
-
     return contorns
-
 
 
 def llegir_matricula(contorns,model,model_n,model_ll):
@@ -134,15 +121,10 @@ def llegir_matricula(contorns,model,model_n,model_ll):
             cv2.imwrite('tempn.jpg', numero)
             img = tf.keras.preprocessing.image.load_img('tempn.jpg', target_size=(128, 64))
             img_array = tf.keras.preprocessing.image.img_to_array(img)
-            img_array = img_array.flatten().reshape(1, -1)
-           
+            img_array = img_array.flatten().reshape(1, -1)        
             predictions = model_n.predict(img_array)
-            predicted_n = predictions[0]
-            
-            
-            resultat1.append(str(predicted_n))
-        
-    
+            predicted_n = predictions[0]         
+            resultat1.append(str(predicted_n)) 
         for i, contorn in enumerate(lletres):
             x, y, w, h = cv2.boundingRect(contorn)
             lletra = matricula[y:y+h, x:x+w]
@@ -154,15 +136,9 @@ def llegir_matricula(contorns,model,model_n,model_ll):
             img = tf.keras.preprocessing.image.load_img('templl.jpg', target_size=(128, 64))
             img_array = tf.keras.preprocessing.image.img_to_array(img)
             img_array = img_array.flatten().reshape(1, -1)
-
             predictions = model_ll.predict(img_array)
-            predicted_class = predictions[0]
-            
-            resultat1.append(str(predicted_class))
-        
-         
-        
-    
+            predicted_class = predictions[0]         
+            resultat1.append(str(predicted_class))   
         for i, contorn in enumerate(contorns):
             x, y, w, h = cv2.boundingRect(contorn)
             lletra = matricula[y:y+h, x:x+w]
@@ -173,52 +149,39 @@ def llegir_matricula(contorns,model,model_n,model_ll):
             cv2.imwrite('templl.jpg', lletra)
             img = tf.keras.preprocessing.image.load_img('templl.jpg', target_size=(128, 64))
             img_array = tf.keras.preprocessing.image.img_to_array(img)
-            img_array = img_array.flatten().reshape(1, -1)
-            
+            img_array = img_array.flatten().reshape(1, -1)        
             predictions = model.predict(img_array)
-            predicted_class = predictions[0]
-                
-            resultat2.append(str(predicted_class))
-        
-         
-        
+            predicted_class = predictions[0]              
+            resultat2.append(str(predicted_class))      
         return ''.join(resultat1),''.join(resultat2)
+
 
 def llegir_matriculacnn(contorns,model, model_n, model_ll):
     resultat1 = []
     resultat2 = []
     numeros = contorns[:4]
-    lletres = contorns[-3:]
-    
-    
+    lletres = contorns[-3:]   
     for i, contorn in enumerate(numeros):
         x, y, w, h = cv2.boundingRect(contorn)
         numero = matricula[y:y+h, x:x+w]
         cv2.imwrite('tempn.jpg', numero)
-
         img = tf.keras.preprocessing.image.load_img('tempn.jpg', target_size=(128, 64))
         img_array = tf.keras.preprocessing.image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)  # Expandir a batch size 1
-
         prediccion_numero = model_n.predict(img_array)
         predicted_number = np.argmax(prediccion_numero, axis=1)[0]  # Clase predicha
         resultat1.append(str(predicted_number))
-    
-    
     for i, contorn in enumerate(lletres):
         x, y, w, h = cv2.boundingRect(contorn)
         lletra = matricula[y:y+h, x:x+w]
         cv2.imwrite('templl.jpg', lletra)
-
         img = tf.keras.preprocessing.image.load_img('templl.jpg', target_size=(128, 64))
         img_array = tf.keras.preprocessing.image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)
-
         prediccion_lletra = model_ll.predict(img_array)
         predicted_letter = np.argmax(prediccion_lletra, axis=1)[0]  # Clase predicha
         classes = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z']
-        resultat1.append(str(classes[predicted_letter]))
-        
+        resultat1.append(str(classes[predicted_letter]))        
     for i, contorn in enumerate(contorns):
         x, y, w, h = cv2.boundingRect(contorn)
         lletra = matricula[y:y+h, x:x+w]
@@ -230,19 +193,13 @@ def llegir_matriculacnn(contorns,model, model_n, model_ll):
         img = tf.keras.preprocessing.image.load_img('templl.jpg', target_size=(128, 64))
         img_array = tf.keras.preprocessing.image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)
-
         predictions = model.predict(img_array)
-        predicted_letter = tf.argmax(predictions[0]).numpy()
-        
+        predicted_letter = tf.argmax(predictions[0]).numpy()        
         ll=['0','1','2','3','4','5','6','7','8','9','B','C','D','F','G','H','J','K','L','M','N','P','R','S','T','V','W','X','Y','Z']
-
-        resultat2.append(str(ll[predicted_letter]))
-    
+        resultat2.append(str(ll[predicted_letter]))    
     return ''.join(resultat1), ''.join(resultat2)
 
 
-
-import joblib
 
 modelcnn = load_model('model_general2.h5')
 modelsvm=joblib.load('svm_model_general.pkl')
